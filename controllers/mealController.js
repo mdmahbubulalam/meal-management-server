dcfa5097123a0ac2da9f29d1e3779c5c18598a34
mdmahbubulalam <alam.diu13@gmail.com>
2023-06-28 19:55:49 +0600

    added single meal route

--- controllers/mealController.js
+++ controllers/mealController.js
@@ -44,6 +44,26 @@ exports.getAllMeal = async (req, res, next) => {
     }
 }
 
+exports.singleMeal = async (req, res, next) => {
+    const mealId = await req.params.mealId;
+    try {
+
+        const meal = await Meal.findById(mealId)
+        if (!meal) {
+            res.status(400).json({message: "Wrong meal!"});
+       }
+
+       res.status(200).json({
+        message:"Successfully get meal",
+        meal
+    });
+        
+    } catch (err) {
+        console.log(err)
+        res.status(401).json({message: "You can't get the meal"});
+    }
+}
+
 exports.userMealInfo = async (req, res, next) => {
     const {email, monthName} = await req.query;
     
--- routes/mealRoute.js
+++ routes/mealRoute.js
@@ -1,8 +1,9 @@
-const { getAllMeal, addMeal, updateMeal, deleteMeal, userMealInfo, currentMonthMealInfo } = require('../controllers/mealController');
+const { getAllMeal, addMeal, singleMeal, updateMeal, deleteMeal, userMealInfo, currentMonthMealInfo } = require('../controllers/mealController');
 
 const mealRoute = require('express').Router();
 
 mealRoute.get('/allMeals', getAllMeal)
+mealRoute.get('/:mealId', singleMeal)
 mealRoute.post('/addMeal', addMeal)
 mealRoute.get('/userMealInfo', userMealInfo)
 mealRoute.get('/currentMonthMealInfo', currentMonthMealInfo)
